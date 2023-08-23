848a39639f4101cbc68642e10672c60a7a9b3b55
Gabriel W.K <gabrielwernerkuhn1@gmail.com>
2023-08-23 09:12:11 -0300

    estrutura do app | inicio lógica dos pontos

--- lib/Model/frame.dart
+++ lib/Model/frame.dart
@@ -1,28 +1,34 @@
 class Frame {
   int index;
-  int square1;
-  int square2;
+  String square1;
+  String square2;
+  String pontuation;
+  bool spare;
+  bool strike;
 
-  Frame (this.index, this.square1, this.square2);
+  Frame (this.index, this.square1, this.square2, this.pontuation, this.spare, this.strike);
 
 // === GETTERS =================================================\\
   String getIndex () {
     return index.toString();
   }
-  String getSquare1 () {
-    return square1.toString();
+  int getSquare1 () {
+    return int.parse(square1);
   }
-  String getSquare2 () {
-    return square2.toString();
+  int getSquare2 () {
+    return int.parse(square2);
+  }
+  int getPontuation () {
+    return int.parse(square1) + int.parse(square2);
   }
 // === SETTERS =================================================\\
   void setIndex (int newIndex) {
     index = newIndex;
   }
-  void setSquare1 (int point) {
+  void setSquare1 (String point) {
     square1 = point;
   }
-  void setSquare2 (int point) {
+  void setSquare2 (String point) {
     square2 = point;
   }
 }
--- lib/View/home_page.dart
+++ lib/View/home_page.dart
@@ -22,6 +22,19 @@ class _HomePageState extends State<HomePage> {
     [10, 0],           // Frame 9: Strike
     [10, 10, 10]    // Frame 10: Three Strikes
   ];
+
+  List<Frame> listaDeFrames = [
+    Frame(1, "10", " ", "10", false, true),
+    Frame(2, "10", " ", "10", false, true),
+    Frame(3, "10", " ", "10", false, true),
+    Frame(4, "10", " ", "10", false, true),
+    Frame(5, "10", " ", "10", false, true),
+    Frame(6, "10", " ", "10", false, true),
+    Frame(7, "10", " ", "10", false, true),
+    Frame(8, "10", " ", "10", false, true),
+    Frame(9, "10", " ", "10", false, true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,56 +48,80 @@ class _HomePageState extends State<HomePage> {
           },
         ),
       ),
-      body: ListView.builder(
-        scrollDirection: Axis.horizontal, 
-        itemCount: (10),
-        itemBuilder: (BuildContext context, int index){
-          return Padding(
-            padding: const EdgeInsets.all(10),
-            child: Center(
-              child: DecoratedBox(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 5,
-                    ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: (10),
+              itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    children: [
+                      FloatingActionButton(
+                        backgroundColor: Colors.amberAccent,
+                        foregroundColor: const Color.fromARGB(255, 89, 71, 7),
+                        elevation: 6,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+                        onPressed: () {
+                          // adicionar os pontos ao respectivo frame \\
+                        },
+                        child: Text((index+1).toString()),
+                      )
+                    ],
                   ),
-                child: GestureDetector(
-                  onTap: () {
-                    // adicionar o valor ao respectivo frame 
-                    debugPrint("valor ${(index+1)} Adicionado");
-                  },
-                  child: Container(
-                    width: 60,
-                    height: 80,
-                    color: Colors.amber,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text((index+1).toString()),
-                        ])
-                  ),
-                ),
-              ),
+                );
+              },
             ),
-          );
-        }
-
-      ),
+          ),
+          
+          Expanded(
+            child: ListView.builder(
+            scrollDirection: Axis.horizontal, 
+            itemCount: (9),
+            itemBuilder: (BuildContext context, int index){
+              Frame frameAtual = listaDeFrames[index];
+              return Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Text(frameAtual.getIndex()),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 5,
+                        ),
+                      ),
+                      child:Container(
+                        width: 70,
+                        height: 70,
+                        color: Colors.amber,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(frameAtual.square1),
+                              Text(frameAtual.square2)
+                            ]),
+                            Padding(
+                              padding: const EdgeInsets.all(15),
+                              child: Text(frameAtual.pontuation))
+                          ]
+                        )
+                      ),
+                    ),
+                  ]
+                  ),
+                );
+            }
+                
+                  ),
+          ),
+      ]),
     );
   }
-
-  int pointsCalculator (List<int> frame){
-    int sum = frame[0] + frame[1];
-
-    if (sum < 10)
-      return sum;
-    
-    if (frame[0] == 10)
-      return 11;
-
-    return 12;
-  }
-
-
 }
